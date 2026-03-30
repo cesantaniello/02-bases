c6f40174437f148a31497223555d3ff1589c14aa
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-03-30 14:13:51 +0200

    Signals

--- src/app/pages/counter/app.templateUrl.ts
+++ src/app/pages/counter/app.templateUrl.ts
@@ -1,15 +1,21 @@
+import { signal } from "@angular/core";
+
 export class CounterLogic {
   counter: number = 20;
+  counterSignal = signal(10);
 
   resetCounter() {
     this.counter = 0;
+    this.counterSignal.set(0);
   }
 
   increaseBy(value: number) {
     this.counter += value;
+    this.counterSignal.update(current => current + value);
   }
 
   decreaseBy(value: number) {
     this.counter -= value;
+    this.counterSignal.update(current => current - value);
   }
 }
