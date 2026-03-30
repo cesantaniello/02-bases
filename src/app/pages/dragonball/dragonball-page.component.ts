dec81816b5cfbafa8f6f52a3347da779fbbfb3bd
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-03-30 17:37:09 +0200

    Use @for to iterate

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
 @Component({
   templateUrl: './dragonball-page.component.html',
 })
-export class DragonballPageComponent {
 
+export class DragonballPageComponent {
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9000 },
+    { id: 2, name: 'Vegeta', power: 8500 },
+    { id: 3, name: 'Gohan', power: 7000 },
+    ]
+  );
 }
