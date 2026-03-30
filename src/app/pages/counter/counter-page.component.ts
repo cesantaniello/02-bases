dd880e7da95f8e726f914bd359d0aee7eda8bc78
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-03-30 14:27:22 +0200

    Zoneless

--- src/app/pages/counter/counter-page.component.ts
+++ src/app/pages/counter/counter-page.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 import { CounterLogic } from './app.templateUrl';
 
 @Component({
-  //selector: 'app-counter-page',
   templateUrl: './counter-page.component.html',
-  styleUrls: ['./counter-page.styles.css']
+  styleUrls: ['./counter-page.styles.css'],
 })
 
 export class CounterPageComponent extends CounterLogic {}
