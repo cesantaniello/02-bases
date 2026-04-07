37661e9ecc5ab10ddf7c98224dd087911da17626
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-04-07 15:02:07 +0200

    Use effects and LocalStorage

--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -1,15 +1,31 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class DragonballService {
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9000 },
-    { id: 2, name: 'Vegeta', power: 8500 },
-  ]);
+  characters = signal<Character[]>(this.loadFromLocalStorage());
 
-  addCharacter(character: Character) {
-    this.characters.update(chars => [...chars, { ...character, id: Math.max(...chars.map(c => c.id)) + 1 }]);
+  saveToLocalStorage = effect(() => {
+    console.log(
+      `Saving characters to local storage: ${this.characters().length}`,
+    );
+    localStorage.setItem('characters', JSON.stringify(this.characters()));
+  });
+
+  private loadFromLocalStorage(): Character[] {
+    const stored = localStorage.getItem('characters');
+    return stored
+      ? JSON.parse(stored)
+      : [
+          { id: 1, name: 'Goku', power: 9000 },
+          { id: 2, name: 'Vegeta', power: 8500 },
+        ];
   }
 
+  addCharacter(character: Character) {
+    this.characters.update((chars) => [
+      ...chars,
+      { ...character, id: Math.max(...chars.map((c) => c.id)) + 1 },
+    ]);
+  }
 }
